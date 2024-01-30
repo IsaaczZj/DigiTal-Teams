d29477c9114faba4d077f88a00c84ca43ef3ae42
IsaaczZj <joseisaac.isaac123@gmail.com>
2024-01-30 12:08:54 -0300

    js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,32 @@
 criarBtn.onclick = () => {
-    // alert("Clicou")
-    console.log("Clicou rapaz")
-    overlay.classList.add('show')
-    formCriar.classList.add('show')
+  // alert("Clicou")
+  console.log("Clicou rapaz");
+  overlay.classList.add("show");
+  formCriar.classList.add("show");
+};
+fecharBtn.onclick = () => {
+  overlay.classList.remove("show");
+  formCriar.classList.remove("show");
+};
+
+overlay.onclick = () =>{
+    overlay.classList.remove("show");
+    formCriar.classList.remove("show");
 }
+
+formCriar.onsubmit = () => {
+  event.preventDefault();
+  listTeams.innerHTML = " ";
+  listTeams.innerHTML = `<li>
+    <h4>Nome do Team <box-icon name='show'></box-icon></h4>
+    <h1>0 <span>/0</span></h1>
+    <div class="actions">
+    <button>adicionar</button>
+    <button><box-icon name='trash'></box-icon></button>
+    </div>`;
+
+  alert(nome.value);
+
+  overlay.classList.remove("show");
+  formCriar.classList.remove("show");
+};
