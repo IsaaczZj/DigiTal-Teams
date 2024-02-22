dc1eaa663af84aa521e757c20305de8ff8241339
IsaaczZj <joseisaac.isaac123@gmail.com>
2024-02-22 12:15:30 -0300

    update

--- js/index.js
+++ js/index.js
@@ -10,21 +10,33 @@ fecharBtn.onclick = () => {
   overlay.classList.toggle("show");
   formCriar.classList.toggle("show");
 };
+fecharParticipanteBtn.onclick = () =>{
+	overlay.classList.remove('show')
+	formAddParticipante.classList.remove('show')
+}
 
 overlay.onclick = () => {
-  overlay.classList.toggle("show");
-  formCriar.classList.toggle("show");
+  overlay.classList.remove("show");
+  formCriar.classList.remove("show");
+  formAddParticipante.classList.remove('show')
 };
 
 formCriar.onsubmit = () => {
   event.preventDefault();
 
-  teams.push({
-    name: nome.value,
-    capacity: capacidade.value,
-    members: [],
-  });
+  if(verificarLista(nome.value)){
+	
+	alert('Esse nome ja esta em uso')
+  }
+  else{
+	teams.push({
+		name: nome.value,
+		capacity: capacidade.value,
+		members: [],
+	});
+  }
 
+  
   adicionarCard()
   
 
@@ -32,17 +44,61 @@ formCriar.onsubmit = () => {
   formCriar.classList.remove("show");
 };
 
+
+formAddParticipante.onsubmit = () =>{
+	event.preventDefault()
+	teams[Number(teamID.value)].members.push(nomeParticipante.value)
+	alert('Participante inserido com sucesso')
+	formAddParticipante.reset()
+}
+
+
 function adicionarCard() {
   listTeams.innerHTML = " ";
+  if(teams.length ===0){
+	listTeams.innerHTML = '<li class="semTime"><h4>Nenhum time criado</h4></li>'
+	return
+  }
+
   for (let i = 0; i < teams.length; i++) {
     listTeams.innerHTML += `
     <li>
       <h4>${teams[i].name} <box-icon name='show'></box-icon></h4>
       <h1>0/ <span>${teams[i].capacity}</span></h1>
       <div class="actions">
-        <button>adicionar</button>
-        <button><box-icon name='trash'></box-icon></button>
+        <button onclick = "mostrarFormParticipante(${i})">adicionar</button>
+        <button onclick ="removerCard(${i})"><box-icon name='trash'></box-icon></button>
       </div> 
       </li>`;
   }
 }
+
+
+function removerCard(indice){
+	let auxArray = []
+	for(let i = 0; i < teams.length;i++){
+		if(i != indice){
+			auxArray.push(teams[i])
+		}
+
+	}
+	teams = auxArray
+	adicionarCard()
+}
+
+function verificarLista(){
+	let achou = false
+	for(let i = 0;i<teams.length;i++){
+		if(teams[i].name === nomeDoTeams){
+			achou = true
+		}
+	}
+	return achou
+}
+
+function mostrarFormParticipante(indice){
+	overlay.classList.add("show")
+	formAddParticipante.classList.add("show")
+	teamID.value = indice
+}
+
